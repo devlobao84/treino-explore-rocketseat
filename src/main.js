a426b6e0129f2829f8d7cf36e9fbed9c78bd3748
jonas lobo <web@emd.art.br>
2022-10-20 15:11:31 -0300

    validate regex card

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
-
-
 
 function setCardType(type) {
     const colors = {
@@ -20,7 +19,49 @@ function setCardType(type) {
 }
 
 setCardType("mastercard")
-
 // globalThis.setCardType = setCardType
+
+// Código de segurança
+const cvcSecurity = document.getElementById("security-code")
+const securityCodePattern = {
+    mask: "0000"
+}
+const securityCodeMasked = IMask(cvcSecurity, securityCodePattern)
+
+// Datava de validade do cartão
+const expirationDate = document.getElementById("expiration-date")
+const expirationDatePattern = {
+    mask: "00{/}00"
+}
+const expirationDateValidate = IMask(expirationDate, expirationDatePattern)
+
+// Nímero do caortão Visa
+
+const numberCard = document.getElementById("card-number")
+const numberCardPattern = {
+  mask: [
+    {
+      mask: "000 0000 0000 0000",
+      regex: /^4\d{0,15}$/,
+      cardType: "visa",
+    },
+    {
+      mask: "000 0000 0000 0000",
+      regex: /^(5[1-5]\d{0,2}|22[2-9]\d{0,1}|2[3-7]\d{0,2})\d{0,12}/,
+      cardType: "master",
+    },    
+  ],
+}
+
+const numberCardSet = IMask(numberCard, numberCardPattern)
+
+
+
+
+
+
+
+
+
 
 
