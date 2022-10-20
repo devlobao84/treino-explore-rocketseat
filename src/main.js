0f1c6fb17591a00662c215aa23880d440638b987
jonas lobo <web@emd.art.br>
2022-10-20 11:12:10 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+
+
+function setCardType(type) {
+    const colors = {
+        "visa": ["#2D57F2", "#436D99", "#AF1DE2"],
+        "mastercard": ["#C69347", "#DF6F29", "#DF2929"],
+        "default": ["black", "gray"],
+    }
+    
+    ccBgColor01.setAttribute("fill", colors[type] [0] )
+    ccBgColor02.setAttribute("fill", colors[type] [1] )
+    ccBgColor02.setAttribute("fill", colors[type] [2] )
+    ccLogo.setAttribute("src",`cc-${type}.svg`)
+}
+
+setCardType("mastercard")
+
+// globalThis.setCardType = setCardType
+
+
